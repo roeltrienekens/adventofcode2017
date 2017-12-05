3b3a1b6f23d0c36203e41752066f61e60c969f17
roeltrienekens <roel.trienekens@gmail.com>
2017-12-05 17:50:02 +0100

    Cleanup Day 5b.

--- day5/5b.py
+++ day5/5b.py
@@ -1,10 +1,7 @@
+import unittest
+
 inputfile = "in4a.txt"
 testfile = "in4a_test.txt"
-# test2file = ""
-
-#mode = 2
-
-import unittest
 
 instr = []
 
@@ -29,9 +26,6 @@ def jump_inc_dec(instr, loc):
     return new_loc
 
 def isExit(instr, loc):
-#    print(str(loc) + " " + str(loc + instr[loc]) + " " + str(len(instr) - 1))
-#    if loc < 0:
-#        return False
     if (loc + instr[loc] > (len(instr) - 1)):
         return True
     else:
@@ -41,13 +35,9 @@ def traverse(instr): #, mode):
     steps = 0
     loc = 0
     while isExit(instr, loc) == False:
-#        if mode == 1:
-#            loc = jump_inc(instr, loc)
-#        elif mode == 2:
         loc = jump_inc_dec(instr, loc)
         steps += 1
     return steps + 1
-
 
 # Unit tests
 
@@ -55,13 +45,15 @@ class MyTest(unittest.TestCase):
     def test(self):
 
         test_instr = [0, 3, 0, 3, -3]
-        assertFalse(isExit(test_instr, 0))
-        assertFalse(isExit(test_instr, 1))
-        assertFalse(isExit(test_instr, 2))
-        assertTrue(isExit(test_instr, 3))
-        assertFalse(isExit(test_instr, 4))
+        self.assertFalse(isExit(test_instr, 0))
+        self.assertFalse(isExit(test_instr, 1))
+        self.assertFalse(isExit(test_instr, 2))
+        self.assertTrue(isExit(test_instr, 3))
+        self.assertFalse(isExit(test_instr, 4))
 
 if __name__ == '__main__':
+
+
 
 #    read_instr(testfile)
 #    print(traverse(instr, 1))
@@ -81,4 +73,4 @@ if __name__ == '__main__':
     read_instr(inputfile)
     print(traverse(instr)) #, mode))
 
-#    unittest.main()
+    unittest.main()
