77bb583280d7ed78569a45063e743a44c2a42de5
roeltrienekens <roel.trienekens@gmail.com>
2017-12-22 15:42:07 +0100

    Added the (admittedly low-tech) solution for day 19a

---
+++ day19/19a.py
@@ -0,0 +1,4 @@
+#   Wondered if you can solve this by following the line around on the screen. It turns out you can.
+#   Then, decided to code the solution as well, following the 'YAGNI' principle.
+
+print('DTOUFARJQ')
